f8a1170e9e59e9d88f0288bc0d06449a76401863
Guilherme Pinto <guilhermecamachop@gmail.com>
2018-09-24 20:22:02 -0300

    Dump da base

--- backend/mysqldump.sql
+++ backend/mysqldump.sql
@@ -1,33 +1,76 @@
 /*
  Navicat MySQL Data Transfer
 
- Source Server         : my vps
+ Source Server         : Local
  Source Server Type    : MySQL
  Source Server Version : 50723
- Source Host           : 173.82.245.98:3306
- Source Schema         : quality_test
+ Source Host           : localhost:8889
+ Source Schema         : lojinhadoze
 
  Target Server Type    : MySQL
  Target Server Version : 50723
  File Encoding         : 65001
 
- Date: 08/09/2018 09:57:43
+ Date: 24/09/2018 20:21:40
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for tasks
+-- Table structure for category
 -- ----------------------------
-DROP TABLE IF EXISTS `tasks`;
-CREATE TABLE `tasks` (
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `tags` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `state` varchar(255) DEFAULT NULL,
-  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` int(65) NOT NULL AUTO_INCREMENT,
+  `user_id` int(65) DEFAULT NULL,
+  `product_id` int(65) DEFAULT NULL,
+  `quantity` int(65) DEFAULT NULL,
+  `timestamp` timestamp(6) NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(6),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for product
+-- ----------------------------
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `id` int(65) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `features` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(65) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `social_id` varchar(255) DEFAULT NULL,
+  `zip_code` varchar(255) DEFAULT NULL,
+  `zip_number` varchar(255) DEFAULT NULL,
+  `zip_additional` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
