1c1200bef45c7a1dc6ee7ee90996a45ee82a0c59
Guilherme Pinto <guilhermecamachop@gmail.com>
2018-09-26 16:28:40 -0300

    agora vai mizeravel

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'http://lojinhadozeapi.guilhermecamacho.com/',
+  apikey: 'Basic bG9qaW5oYWRvemU6Vzckc09TU3BGcyR4ZTdVIUs2UlokWFlYaUg1ZG1l'
 };
